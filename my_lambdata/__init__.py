d32277489585c476db18994c022b08f4876a5828
BoWarburton <bo@warburton.com>
2020-05-05 20:56:13 -0700

    Added __init__.py git log

---
+++ my_lambdata/__init__.py
@@ -0,0 +1,2 @@
+# alternatively invoke Python modularly
+# python -m my_lambdata.my_script
---
+++ my_lambdata/my_mod.py
@@ -0,0 +1,10 @@
+# first function
+
+def enlarge(n):
+    return n * 100
+
+x = 5
+print(enlarge(x))
+
+y = int(input('Please choose a numbe (e.g. 5): '))
+print(enlarge(y))
