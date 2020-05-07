4dab93d9e001bb6b9b191eb8ad7f5a80205125c2
BoWarburton <bo@warburton.com>
2020-05-07 15:02:10 -0700

    Changes repo name

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,13 +1,18 @@
-# my_mod.py
+# Parameter n is a number
+# return 100 times parameter
+
+# This breaks ability to import function enlarge from other files
+# if left in global scope:
+# print('Hello, world')
+# y = int(input('Please input a number '))
+# print(y, enlarge(y))
 
 def enlarge(n):
     return n * 100
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # only run the code below if executing this script from the command line
     # otherwise don't run it
-    x = 5
-    print(enlarge(x))
-
-y = int(input('Please choose a numbe (e.g. 5): '))
-print(enlarge(y))
+    print('Hello!')
+    y = int(input('Please choose a number: '))
+    print(y, enlarge(y))
--- my_lambdata/setup.py
+++ my_lambdata/setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my-package-name", # the name that you will install via pip
+    name="my-lambdata-dspt5-bowarburton", # the name that you will install via pip
     version="1.0",
-    author="Your Full Name",
-    author_email="your@email.com",
-    description="A short description",
+    author="Bo Warburton",
+    author_email="bo@warburton.com",
+    description="Lambda School Data Science Unit 3, Sprint 1",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/YOUR_USERNAME/YOUR_REPO_NAME",
+    url="https://github.com/bowarburton/lambdata-dspt5-bowarburton",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
