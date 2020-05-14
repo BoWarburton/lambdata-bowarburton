39e7f3b646b20535f98dfefeeafdcd78f66e21ed
BoWarburton <bo@warburton.com>
2020-05-14 16:38:00 -0700

    Added setup.py

--- my_lambdata/setup.py
+++ my_lambdata/setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my-lambdata-dspt5-bowarburton", # the name that you will install via pip
+    name="lambdata-bowarburton",
     version="1.0",
     author="Bo Warburton",
     author_email="bo@warburton.com",
-    description="Lambda School Data Science Unit 3, Sprint 1",
+    description="Lambda School Data Science Cohort DSPT5, Unit 3, Sprint 1",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
-    url="https://github.com/bowarburton/lambdata-dspt5-bowarburton",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/bowarburton/lambdata-bowarburton",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
