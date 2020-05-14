8a5978296272121bbef79d66a74c1c41aa3a3e8c
BoWarburton <bo@warburton.com>
2020-05-14 16:16:20 -0700

    Create input

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -10,9 +10,15 @@
 def enlarge(n):
     return n * 100
 
-if __name__ == '__main__':
-    # only run the code below if executing this script from the command line
-    # otherwise don't run it
-    print('Hello!')
-    y = int(input('Please choose a number: '))
-    print(y, enlarge(y))
+x = 5
+print(enlarge(x))
+
+y = int(input('Please choose a number: '))
+print(enlarge(y))
+
+# if __name__ == '__main__':
+#     # only run the code below if executing this script from the command line
+#     # otherwise don't run it
+#     print('Hello!')
+#     y = int(input('Please choose a number: '))
+#     print(y, enlarge(y))
